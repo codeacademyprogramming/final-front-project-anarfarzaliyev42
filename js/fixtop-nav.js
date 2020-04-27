e6f62288c782b6d2019fd71ee53dd3196cd31361
anarfarzaliyev9 <anarfarzaliyev9@gmail.com>
2020-04-27 15:47:37 +0400

    add

--- js/fixtop-nav.js
+++ js/fixtop-nav.js
@@ -1,10 +1,12 @@
 $(function () {
-    var fixmeTop = $(".main-nav").offset().top; // get initial position of the element
+    var fixmeTop = $(".main-nav").offset().top; 
+    // get initial position of the element
 
     $(window).scroll(function () {
       // assign scroll event listener
   
-      var currentScroll = $(window).scrollTop(); // get current position
+      var currentScroll = $(window).scrollTop(); 
+      // get current position
   
       if (currentScroll >= fixmeTop) {
         // apply position: fixed 
